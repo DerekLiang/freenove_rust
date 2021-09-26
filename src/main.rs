245c1f0667910908b8e6f1359e7a7279af52d2da
Derek Liang <derek.liang@worksafebc.com>
2021-09-25 22:29:40 -0700

    soft pwm for 3 RGB LED

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::thread;
 use std::time::Duration;
+use rand::Rng;
 
 use rppal::gpio::Gpio;
 use rppal::gpio::Level;
@@ -11,16 +12,36 @@ use rppal::pwm::{Channel, Polarity, Pwm};
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
 
+    let mut pin2 = Gpio::new()?.get(17)?.into_output();
+    let mut pin3 = Gpio::new()?.get(27)?.into_output();
+
     // Enable PWM channel 0 (BCM GPIO 18, physical pin 12) at 2 Hz with a 25% duty cycle.
-    let pwm = Pwm::with_frequency(Channel::Pwm0, 2.0, 0.25, Polarity::Normal, true)?;
+    let pwm1 = Pwm::with_frequency(Channel::Pwm0, 2.0, 0.25, Polarity::Normal, true)?;
+    // let pwm2 = Pwm::with_frequency(Channel::Pwm1, 2.0, 0.25, Polarity::Normal, true)?;
 
-    // Sleep for 2 seconds while the LED blinks.
-    thread::sleep(Duration::from_secs(2));
+    let mut rng = rand::thread_rng();
+    
+    while true {
+        let n1 = rng.gen_range(0.0..1.0);
+        let n2: f32 = rng.gen_range(0.0..1.0);
+        let n3: f32 = rng.gen_range(0.0..1.0);
 
-    // Reconfigure the PWM channel for an 8 Hz frequency, 50% duty cycle.
-    pwm.set_frequency(8.0, 0.5)?;
+        pin2.set_pwm(
+            Duration::from_millis(20),
+            Duration::from_micros(((20000 as f32) * n2) as u64), // wthin 20 ms we take a percentage as the power level.
+        )?;
 
-    thread::sleep(Duration::from_secs(3));
+        pin3.set_pwm(
+            Duration::from_millis(20),
+            Duration::from_micros(((20000 as f32) * n3) as u64),
+        )?;
+
+        
+        pwm1.set_frequency(8.0, n1)?;
+        // pwm2.set_frequency(8.0, n2)?;
+        thread::sleep(Duration::from_secs(1));
+    }
+
 
     Ok(())
 }
