c0d37032f9f3163858a27f045f683ae361c83536
Derek Liang <derek.liang@worksafebc.com>
2021-09-25 21:40:31 -0700

    pwm

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,21 @@ use rppal::gpio::Gpio;
 use rppal::gpio::Level;
 use rppal::gpio::Trigger;
 use rppal::system::DeviceInfo;
-
-const GPIO_LEDS: [u8;10] = [17, 18, 27, 22, 23, 24, 25, 2, 3, 8];   
+use rppal::pwm::{Channel, Polarity, Pwm};
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
 
-    
-    while true {      
-        for &led in GPIO_LEDS.iter() {
-            
-            let mut led_pin = Gpio::new()?.get(led)?.into_output();
-            led_pin.set_low();
-            thread::sleep(Duration::from_millis(100));
-            led_pin.set_high();
-            thread::sleep(Duration::from_millis(100));
-        }
-    }
+    // Enable PWM channel 0 (BCM GPIO 18, physical pin 12) at 2 Hz with a 25% duty cycle.
+    let pwm = Pwm::with_frequency(Channel::Pwm0, 2.0, 0.25, Polarity::Normal, true)?;
+
+    // Sleep for 2 seconds while the LED blinks.
+    thread::sleep(Duration::from_secs(2));
+
+    // Reconfigure the PWM channel for an 8 Hz frequency, 50% duty cycle.
+    pwm.set_frequency(8.0, 0.5)?;
+
+    thread::sleep(Duration::from_secs(3));
 
     Ok(())
 }
