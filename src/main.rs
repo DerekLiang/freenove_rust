412efe2d997f39656259db8995489e9828ec40ea
Derek Liang <derek.liang@worksafebc.com>
2021-09-26 18:02:40 -0700

    buzzer is working

--- src/main.rs
+++ src/main.rs
@@ -12,36 +12,28 @@ use rppal::pwm::{Channel, Polarity, Pwm};
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
 
-    let mut pin2 = Gpio::new()?.get(17)?.into_output();
-    let mut pin3 = Gpio::new()?.get(27)?.into_output();
+    let mut alarm_pin = Gpio::new()?.get(17)?.into_output();
+    let mut switch_pin = Gpio::new()?.get(18)?.into_input_pullup();
 
-    // Enable PWM channel 0 (BCM GPIO 18, physical pin 12) at 2 Hz with a 25% duty cycle.
-    let pwm1 = Pwm::with_frequency(Channel::Pwm0, 2.0, 0.25, Polarity::Normal, true)?;
-    // let pwm2 = Pwm::with_frequency(Channel::Pwm1, 2.0, 0.25, Polarity::Normal, true)?;
-
-    let mut rng = rand::thread_rng();
+    switch_pin.set_interrupt(Trigger::Both)?;
+    alarm_pin.set_low();
+    println!("set almarm pin to low");
     
     while true {
-        let n1 = rng.gen_range(0.0..1.0);
-        let n2: f32 = rng.gen_range(0.0..1.0);
-        let n3: f32 = rng.gen_range(0.0..1.0);
-
-        pin2.set_pwm(
-            Duration::from_millis(20),
-            Duration::from_micros(((20000 as f32) * n2) as u64), // wthin 20 ms we take a percentage as the power level.
-        )?;
-
-        pin3.set_pwm(
-            Duration::from_millis(20),
-            Duration::from_micros(((20000 as f32) * n3) as u64),
-        )?;
-
-        
-        pwm1.set_frequency(8.0, n1)?;
-        // pwm2.set_frequency(8.0, n2)?;
-        thread::sleep(Duration::from_secs(1));
+        let switch = switch_pin.poll_interrupt(false, None)?;
+        match switch {
+            Some(level) => {
+                if level == Level::High {
+                    alarm_pin.set_high();
+                    println!("set almarm pin to high");
+                } else {
+                    alarm_pin.set_low();
+                    println!("set almarm pin to low");
+                }
+            }
+            None => {}
+        }
     }
-
 
     Ok(())
 }
