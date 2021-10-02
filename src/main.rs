f3a910ab9174f9cb24551b186ab30d7c4c6fcd70
Derek Liang <derek.liang@worksafebc.com>
2021-10-01 22:25:00 -0700

    i2c read ads7930

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rppal::i2c::I2c;
 use core::f32::consts::PI;
 use rppal::gpio::OutputPin;
 use std::error::Error;
@@ -12,61 +13,25 @@ use rppal::gpio::Trigger;
 use rppal::system::DeviceInfo;
 use rppal::pwm::{Channel, Polarity, Pwm};
 
-fn alertor( pin: &mut OutputPin  ) -> Result<(), Box<dyn Error>> {
-    for x in 0..360 {
-        let sin_val = (x as f32 * (PI / 180.0)).sin();
-        let ton_val = 2000.0 + sin_val * 1000.0;
-        let value = ((1000 as f32) * ton_val / 3000.0) as u64;
-        // println!("value {}", value);
-        pin.set_pwm(
-            Duration::from_millis(1),
-            Duration::from_micros(value),
-        )?;
-        thread::sleep(Duration::from_micros(100));
-    };    
-    Ok(())
-}
+// run the following command and find the address
+// > i2cdetect -y 1
+const i2cAddress: u16 = 0x4b;
 
-fn stop_alertor(pin: &mut OutputPin)-> Result<(), Box<dyn Error>>  {
-    pin.set_pwm(
-        Duration::from_millis(20),
-        Duration::from_micros(((20000 as f32) * 0.0) as u64),
-    )?;
-    Ok(())
-}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
+    let mut i2c = I2c::new()?;
+    i2c.set_slave_address(i2cAddress)?;
 
-    let mut alarm_pin = Gpio::new()?.get(17)?.into_output();
-    let mut switch_pin = Gpio::new()?.get(18)?.into_input_pullup();
+    // representing different ports, from a0-a7
+    let commands = [0x84, 0xc4, 0x94, 0xd4, 0xa4, 0xe4, 0xb4, 0xf4];
+    let mut reg = [0u8; 1];
 
-    alarm_pin.set_low();
-    println!("set almarm pin to low");
-    
-    let switch_level = Arc::new(Mutex::<Option<Level>>::new(None));
-    let switch_level_data = switch_level.clone();
-
-    switch_pin.set_async_interrupt(Trigger::Both, move |l| {
-        let mut thread_switch_level = switch_level_data.lock().unwrap();
-        *thread_switch_level = Some(l);
-    })?;
-    
     while true {
-        match *switch_level.lock().unwrap() {
-            Some(level) => {
-                if level == Level::High {
-                    stop_alertor(&mut alarm_pin)?;                    
-                } else {
-                    alertor(&mut alarm_pin)?;
-                    println!("set almarm pin to low");
-                }
-            }
-            None => {
-                stop_alertor(&mut alarm_pin)?;
-            }
-        }
-        thread::sleep(Duration::from_micros(1));
+        
+        let adc_value = i2c.block_read(commands[0], &mut reg)?;
+        println!("ADC value : {}, Voltage: {:2}", reg[0],  reg[0] as f32 / 255.0 * 3.3);
+        
+        thread::sleep(Duration::from_secs(1)); 
     }
 
     Ok(())
