ba1552fef4bda72d9717a18bc05cf4d5fc255dff
Derek Liang <derek.liang@worksafebc.com>
2021-10-02 19:19:49 -0700

    Motor & Driver

--- src/main.rs
+++ src/main.rs
@@ -17,38 +17,64 @@ use rppal::system::DeviceInfo;
 // > i2cdetect -y 1
 const i2cAddress: u16 = 0x4b;
 
+fn map(value: i16, from_low: i16, from_high: i16, to_low: i16, to_high: i16) -> i16 {
+    (to_high - to_low) * (value - from_low) / (from_high - from_low) + to_low
+}
+
+fn motor(
+    adc: u8,
+    pin1: &mut OutputPin,
+    pin2: &mut OutputPin,
+    enable_pin: &mut OutputPin,
+) -> Result<(), Box<dyn Error>> {
+    let value = adc as i16 - 128;
+    match value {
+        v if v > 0 => {
+            pin1.set_high();
+            pin2.set_low();
+            println!("turn forward....");
+        }
+        v if v < 0 => {
+            pin1.set_low();
+            pin2.set_high();
+            println!("turn backward....");
+        }
+        _ => {
+            pin1.set_low();
+            pin2.set_low();
+            println!("Motor Stop...");
+        }
+    }
+
+    let duty_cycle = map(value.abs(), 0, 128, 0, 100) as u64;
+    println!("The PWM duty cycle is {}%", duty_cycle);
+
+    enable_pin.set_pwm(
+        Duration::from_millis(20),
+        Duration::from_micros(2000 * duty_cycle / 100),
+    )?;
+
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let mut i2c = I2c::new()?;
     i2c.set_slave_address(i2cAddress)?;
-    let mut switch_pin = Gpio::new()?.get(18)?.into_input_pullup();
 
-    // NOTE: poll_interrupt is not working reliably, using async instead
-    let switch_level = Arc::new(Mutex::<Option<Level>>::new(None));
-    let switch_level_data = switch_level.clone();
+    let mut motor_pin1 = Gpio::new()?.get(27)?.into_output(); // 2
+    let mut motor_pin2 = Gpio::new()?.get(17)?.into_output(); // 0
+    let mut enable_pin = Gpio::new()?.get(22)?.into_output(); // 3
 
-    switch_pin.set_async_interrupt(Trigger::Both, move |l| {
-        let mut thread_switch_level = switch_level_data.lock().unwrap();
-        *thread_switch_level = Some(l);
-    })?;
-
-    while true {
+    loop {
         let commands = [0x84, 0xc4, 0x94, 0xd4, 0xa4, 0xe4, 0xb4, 0xf4];
 
-        let mut reg_y = [0u8; 1];
-        i2c.block_read(commands[0], &mut reg_y)?;
+        let mut adc = [0u8; 1];
+        i2c.block_read(commands[0], &mut adc)?;
 
-        let mut reg_x = [0u8; 1];
-        i2c.block_read(commands[1], &mut reg_x)?;
+        println!("ADC value : {}", adc[0]);
 
-        let val_z = *switch_level.lock().unwrap();
-
-        println!(
-            "val_X: {} ,\tval_Y: {} ,\tval_Z: {:?}",
-            reg_x[0], reg_y[0], val_z
-        );
-
-        thread::sleep(Duration::from_secs(1));
+        motor(adc[0], &mut motor_pin1, &mut motor_pin2, &mut enable_pin)?;
+        thread::sleep(Duration::from_millis(1000));
     }
 
-    Ok(())
 }
