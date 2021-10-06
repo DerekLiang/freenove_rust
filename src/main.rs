be4eafe4742bf38e7d57a3ea2d06718687be3cbd
Derek Liang <derek.liang@worksafebc.com>
2021-10-05 19:20:52 -0700

    refactor to use q_bits

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,19 @@ use rppal::gpio::Trigger;
 use rppal::pwm::{Channel, Polarity, Pwm};
 use rppal::system::DeviceInfo;
 
-// Q0-Q7 q_bit representing 0-7 bit
-fn shift_out(data_pin: &mut OutputPin, clock_pin: &mut OutputPin, q_bit: u8) {
+/// return true if the bit is set for a given u8
+/// bit position start from right to left and staring from 0 to 7
+#[inline]
+fn is_bit_set(bit: u8, bit_pos: u8) -> bool {
+    1 << bit_pos & bit > 0
+}
+
+/// set the q_bits to controller
+/// 0-7 bits of q_bit representing Q0-Q7 output
+fn shift_out(data_pin: &mut OutputPin, clock_pin: &mut OutputPin, q_bits: u8) {
     (0..8).into_iter().rev().for_each(|i| {
         clock_pin.set_low();
-        if i == q_bit {
+        if is_bit_set(q_bits, i) {
             data_pin.set_high();
         } else {
             data_pin.set_low();
@@ -33,7 +41,7 @@ fn set_led(latch_pin: &mut OutputPin, data_pin: &mut OutputPin, clock_pin: &mut 
     shift_out(data_pin, clock_pin, x);
     latch_pin.set_high();
     println!("{}",x);
-    thread::sleep(Duration::from_millis(2000));
+    thread::sleep(Duration::from_millis(200));
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -43,12 +51,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     
     loop {
         (0..8).into_iter().for_each(|x| {
-            set_led(&mut latch_pin,&mut data_pin,&mut clock_pin, x);
+            set_led(&mut latch_pin,&mut data_pin,&mut clock_pin, 1 << x);
         });
         thread::sleep(Duration::from_millis(100));
 
         (0..8).into_iter().rev().for_each(|x| {
-            set_led(&mut latch_pin,&mut data_pin,&mut clock_pin, x);
+            set_led(&mut latch_pin,&mut data_pin,&mut clock_pin, 0x80 >> (7-x));
         });
 
         thread::sleep(Duration::from_millis(100));
